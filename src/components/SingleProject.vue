242a50c1d391cc24ac58e4e9d4c1f9103a1f8ae7
katerina-shche <asti.katerina@gmail.com>
2023-12-03 13:54:20 +0100

    addProject

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class='project' :class='{ complete: project.complete }'>
+    <div class="actions" >
+        <h3 @click='toggleShowDetails'>{{ project.title }}</h3>
+        <div class="icons">
+            <span class="material-icons">edit</span>
+            <span @click='deleteProject' class="material-icons">delete</span>
+            <span @click='toggleComplete' class="material-icons tick">done</span>
+        </div>
+    </div>
+    <div v-if="showDetails">{{ project.details }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [ 'project' ],
+    data() {
+        return {
+            showDetails: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id
+        }
+    },
+    methods: {
+        toggleShowDetails() {
+            this.showDetails = !this.showDetails
+        },
+        deleteProject() {
+            console.log('deleting project')
+            fetch(this.uri, { method: 'DELETE' })
+            .then(() => this.$emit('delete', this.project.id))
+            .catch(err => console.log(err.message))
+        },
+        toggleComplete() {
+            console.log('toggle complete')
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ complete: !this.project.complete })
+            })
+            .then(() => this.$emit('complete', this.project.id))
+            .catch(err => console.log(err.message))
+        }
+    }
+}
+</script>
+ 
+<style>
+    .project {
+        margin: 20px auto;
+        background: white;
+        padding: 10px 20px;
+        border-radius: 4px;
+        box-shadow: 1px 2px 3px rgba(0,0,0,0.5);
+        border-left: 4px solid #e90074;
+    }
+    .project.complete {
+        border-left: 4px solid #00ce89;
+    }
+    .project.complete .tick {
+        color: #00ce89;
+    }
+    h3 {
+        cursor: pointer;
+    }
+    .actions {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .material-icons {
+        font-size: 24px;
+        margin-left: 10px;
+        color: #bbb;
+        cursor: pointer;
+    }
+    .material-icons:hover {
+        color: #777;
+    }
+</style>

fc5e00394925171fbd21b7739278fdde2cdb6abd
katerina-shche <asti.katerina@gmail.com>
2023-12-05 11:36:45 +0100

    editProject

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
+  <Navbar />
   <router-view/>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 
 
 export default {
+  components: { Navbar },
   
   // setup() {
     
---
+++ src/views/EditProject.vue
@@ -0,0 +1,25 @@
+<template>
+<form @submit.prevent="handleSubmit">
+    <label>Title:</label>
+    <input type="text" v-model='title' required>
+    <label>Details:</label>
+    <textarea v-model='details'></textarea>
+    <button>Add Project</button>
+</form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            details: ''
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
